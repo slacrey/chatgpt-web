0bb8422be3de5b441699df123301c4955d6b3b39
slacrey <scstlinfeng@aliyun.com>
2023-05-04 17:47:38 +0800

    修改后端路径

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig((env) => {
   const viteEnv = loadEnv(env.mode, process.cwd()) as unknown as ImportMetaEnv
 
   return {
+    base: '/v1',
     resolve: {
       alias: {
         '@': path.resolve(process.cwd(), 'src'),
