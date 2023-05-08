9863023b72f614d6e5b59a41c9334e55dc3f9640
slacrey <scstlinfeng@aliyun.com>
2023-05-08 13:53:03 +0800

    Revert "修改后端路径"
    This reverts commit bf0447a216a4ccd25478e237bb91804afd6ed975.

--- service/src/index.ts
+++ service/src/index.ts
@@ -716,8 +716,7 @@ router.post('/audit-test', rootAuth, async (req, res) => {
 })
 
 app.use('', router)
-// app.use('/api', router)
-app.use('/v1/api', router)
+app.use('/api', router)
 app.set('trust proxy', 1)
 
 app.listen(3002, () => globalThis.console.log('Server is running on port 3002'))
