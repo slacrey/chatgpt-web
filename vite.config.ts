87950637904fb0cc225a732b9db8ae6e07e09f16
slacrey <scstlinfeng@aliyun.com>
2023-05-08 13:53:05 +0800

    Revert "注册界面隐藏"
    This reverts commit cdf62602f9fc6ee116fac96ef645e3a59feb746a.

--- vite.config.ts
+++ vite.config.ts
@@ -36,15 +36,14 @@ export default defineConfig((env) => {
       port: 1002,
       open: false,
       proxy: {
-        '/v1/api': {
+        '/api': {
           target: viteEnv.VITE_APP_API_BASE_URL,
           changeOrigin: true, // 允许跨域
-          rewrite: path => path.replace('/v1/api/', '/'),
+          rewrite: path => path.replace('/api/', '/'),
         },
       },
     },
     build: {
-      assetsDir: 'v1/static',
       reportCompressedSize: false,
       sourcemap: false,
       commonjsOptions: {
